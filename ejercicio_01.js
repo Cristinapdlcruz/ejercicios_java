33cb311758b7f1ddd945b67eced7e261a42a53cd
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-07-31 01:34:50 +0200

    exercise solving

---
+++ ejercicio_01.js
@@ -0,0 +1,18 @@
+// 1.1
+let myFavoriteHero = 'Hulk'
+
+// 1.2
+let x = 50
+
+// 1.3
+let h = 5
+let y = 10
+
+// 1.4
+let z = h + y
+
+console.log('myFavoriteHero:', myFavoriteHero)
+console.log('x:', x)
+console.log('h:', h)
+console.log('y:', y)
+console.log('z:', z)
