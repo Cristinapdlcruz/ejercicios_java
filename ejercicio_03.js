076ac7a0d113a4e409041a623733551485303024
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-07-31 11:21:03 +0200

    ejercicio 3 resuelto

--- ejercicio_03.js
+++ ejercicio_03.js
@@ -1,20 +1,28 @@
 // 1.1
-console.log(10 * 5)
+let num1 = 10
+let num2 = 5
+
+console.log(`si multiplicamos los dos números da ${num1 * num2}`)
 
 // 1.2
-console.log(10 / 2)
+console.log(`si dividimos el primer núero entre 2 da ${num1 / 2}`)
 
 // 1.3
-console.log(15 % 9)
+let num3 = 15
+let num4 = 9
+console.log(
+  `si dividimos el tercer número con el cuarto de módulo da ${num3 % num4}`
+)
 
 // 1.4
 let p = 10
 let j = 5
-
 let o = p + j
+console.log(`let o es la suma de p y j que da ${o}`)
 
 // 1.5
 let c = 10
 let m = 5
 
 let i = c * m
+console.log(`let i es la multiplicación de c y m que da ${i}`)
