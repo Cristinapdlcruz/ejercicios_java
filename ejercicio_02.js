6fe9aeed08b5fea425fd354f35d88a3ba974560d
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-07-31 11:05:08 +0200

    solucionado ejercicio 1 y 2

--- ejercicio_02.js
+++ ejercicio_02.js
@@ -1,4 +1,5 @@
 // 1.1
+let character = {}
 character.age = 25
 
 // 1.2
@@ -11,9 +12,19 @@ console.log(
 )
 
 // 1.3
-console.log(toy1.price + toy2.price)
+let toy1 = { price: 50 }
+let toy2 = { price: 80 }
+console.log(
+  `El precio total de los dos juguetes es de ${toy1.price + toy2.price}`
+)
 
 // 1.4
-globalBasePrice = 25000
+let globalBasePrice = 30000
+let car1 = { basePrice: 2500, finalPrice: 0 }
+let car2 = { basePrice: 2700, finalPrice: 0 }
+
 car1.finalPrice = car1.basePrice + globalBasePrice
 car2.finalPrice = car2.basePrice + globalBasePrice
+
+console.log(`El precio final del primer coche es de ${car1.finalPrice}`)
+console.log(`El precio final del segundo coche es de ${car2.finalPrice}`)
