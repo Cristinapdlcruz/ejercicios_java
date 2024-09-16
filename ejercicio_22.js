99ecf6ac0b283cb1616aba82ec5c65bd7a88e8f2
Cristinapdlcruz <cristinapdlcruz@gmail.com>
2024-09-16 13:32:00 +0200

    finished

--- ejercicio_22.js
+++ ejercicio_22.js
@@ -7,3 +7,11 @@ const foodSchedule = [
   { name: 'Rice', isVegan: true },
   { name: 'Pasta', isVegan: true }
 ]
+
+for (let i = 0; i < foodSchedule.length; i++) {
+  if (foodSchedule[i].isVegan === false) {
+    foodSchedule[i].name = fruits.splice(0, 1)[0]
+    foodSchedule.isVegan = true
+  }
+}
+console.log(foodSchedule)
